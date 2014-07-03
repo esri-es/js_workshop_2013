82a16ba2861fe2afaedc832c02673025377ccaea
Raul Jimenez Ortega <hhkaos@gmail.com>
2014-07-03 12:44:17 +0200

    Limpiando el código de comentarios y tabulando un poco

--- 10_jquery/js/app.js
+++ 10_jquery/js/app.js
@@ -3,13 +3,31 @@
 var map, geocoder;
 
 require([
-	'esri/map', 'esri/dijit/Geocoder', 'esri/layers/FeatureLayer', 'esri/InfoTemplate', 'esri/layers/ArcGISImageServiceLayer', 'esri/layers/WebTiledLayer','esri/layers/ArcGISTiledMapServiceLayer',
-	'esri/dijit/Popup',	'esri/dijit/InfoWindow',
+	'esri/map', 
+	'esri/dijit/Geocoder', 
+	'esri/layers/FeatureLayer', 
+	'esri/InfoTemplate', 
+	'esri/layers/ArcGISImageServiceLayer', 
+	'esri/layers/WebTiledLayer',
+	'esri/layers/ArcGISTiledMapServiceLayer',
+	'esri/dijit/Popup',	
+	'esri/dijit/InfoWindow',
 	'dojo/dom-construct',
-	'jquery', 'dojo/domReady!'], 
-function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, WebTiledLayer, ArcGISTiledMapServiceLayer,
-	Popup, InfoWindow, domConstruct, $)
-{			
+	'jquery', 
+	'dojo/domReady!'
+],function(
+	Map, 
+	Geocoder, 
+	FeatureLayer, 
+	InfoTemplate, 
+	ArcGISImageServiceLayer, 
+	WebTiledLayer, 
+	ArcGISTiledMapServiceLayer,
+	Popup, 
+	InfoWindow, 
+	domConstruct, 
+	$
+){			
 	map = new Map("map",{
 		basemap: "satellite",
 		center: [0.373, 42.382],
@@ -22,8 +40,6 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 
 	map.addLayer( new WebTiledLayer('http://${subDomain}.tile.stamen.com/toner-lines/${level}/${col}/${row}.png',{ subDomains: ["a","b","c","d"]}) );
 	map.addLayer( new WebTiledLayer('http://${subDomain}.tile.stamen.com/toner-labels/${level}/${col}/${row}.png',{ subDomains: ["a","b","c","d"]}) );
-	// map.addLayer( new WebTiledLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/${level}/${row}/${col}.png'));
-	// map.addLayer( new ArcGISTiledMapServiceLayer('http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer'));
 
 	var popup = new Popup({
 		popupWindow: false
@@ -48,10 +64,6 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 		console.log('set-features');
 	});
 
-	// var infoWindow = new InfoWindow({}, dojo.create("div",null,map.root));
-	// infoWindow.startup();
-	// map.setInfoWindow(infoWindow);
-
 	var infoTemplate = new InfoTemplate();
 	infoTemplate.setTitle("${name}");
 	infoTemplate.setContent(getWeatherFor);
@@ -62,7 +74,6 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 		infoTemplate: infoTemplate
 	});
 	map.addLayer( featureLayer );
-
 
 	geocoder = new Geocoder({ 
 		map: map 
@@ -76,24 +87,7 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 		$('#map').fadeTo(1000,1);
 		$('#search').delay(500).fadeTo(500,1);
 
-		/*
-		map.on('click', function(evt)
-		{
-			if( evt.graphic )
-			{
-				console.log(evt.graphic);
-				$('#info').text(evt.graphic.attributes.name || 'n/a');
-				$('#info').fadeTo(300,1);
-
-				getWeatherFor(evt.graphic);
-			}
-			else
-			{
-				$('#info').fadeOut(300);
-			}
-		}); // map.on(click)
-		*/
-	}); // map.on(load)
+	});
 
 	function showInfo(feature)
 	{
@@ -123,12 +117,10 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 		
 		req.then(function(data)
 		{
-			//http://dribbble.com/shots/912994-Weather-Icon-Update
 			console.log(data);
 			var summary = $('<div>').html(data.hourly.summary);
 			$('#info').append(summary).hide().slideDown('slow');
 
-			//deferred.callback('ok');
 			deferred.callback(data.hourly.summary);
 		},
 		function(error)
@@ -139,5 +131,4 @@ function(Map, Geocoder, FeatureLayer, InfoTemplate, ArcGISImageServiceLayer, Web
 
 		return deferred;
 	}
-
 });
